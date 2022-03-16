b846c81c9809e6f03decaffee10bcc2dff158eef
maaravin <maaravin@LIN24008358.corp.capgemini.com>
2022-03-16 10:35:49 +0530

    added one program...

---
+++ src/com/demo1/Test1.java
@@ -0,0 +1,17 @@
+package com.demo1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		char  a ='A';
+		if(a=='N') {
+			System.out.println("True"); //add elseif condition here
+			
+		}
+		else {
+			System.out.println("false");
+		}
+
+	}
+
+}
