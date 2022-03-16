b791d5bd838902a638560ba44da994082f5f6000
zuveriy <96325175+zuveriy@users.noreply.github.com>
2022-03-16 10:58:56 +0530

    added elseif condition

---
+++ src/com/demo1/test2.java
@@ -0,0 +1,25 @@
+ 
+package com.demo1;
+
+public class test2{
+
+	public static void main(String[] args) {
+		char  a ='A';
+		if(a=='N') {
+			System.out.println("True"); //add elseif condition here
+			
+		}
+		else if (a=='y'){
+			System.out.println("not equal");
+		}
+		
+		else {
+			System.out.println("false");
+		}
+		
+		
+
+	}
+	
+
+}
